bb663377da0ad3129a0cd82f51a7c7f54845f3ba
Pradhanya21 <pradhanyars13@gmail.com>
2024-11-12 21:38:46 +0530

    updated requirements.txt

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def predict():
     return render_template('index.html', prediction_text=f'Diabetes Prediction: {prediction_text}', bmi_category=f'BMI Category: {bmi_category}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
